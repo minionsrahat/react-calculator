4c5b29266c7de723e74728c8e435b590fa193f7e
minionsrahat <rahatuddin786@gmail.com>
2022-04-14 15:26:02 +0600

    Project init and css design completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+
+   <div class="container mt-5">
+     <div class="row my-5">
+       <div class="col-8 mx-auto">
+
+         <div className="grid-container">
+           <div className="output">
+             <div className="previous-operend">
+               12345
+             </div>
+             <div className="current-operend">
+               6789
+             </div>
+           </div>
+           <button className='span-2'>AC</button>
+             <button>DEL</button>
+             <button>÷</button>
+             <button>1</button>
+             <button>2</button>
+             <button>3</button>
+             <button>+</button>
+             <button>4</button>
+             <button>5</button>
+             <button>6</button>
+             <button>-</button>
+             <button>7</button>
+             <button>8</button>
+             <button>9</button>
+             <button>*</button>
+             <button>.</button>
+             <button>0</button>
+             <button className='span-2'>=</button>     
+         </div>
+         
+       </div>
+       
+     </div>
+   </div>
+   </>
   );
 }
 
